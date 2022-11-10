0d1b174245de8b69eeed3254465644901411bfe3
Alessandro Tome <ALESSANDRO.TOME@local.ittcesena.it>
2022-11-10 09:18:05 +0100

    CD 1.1

---
+++ ProvaProvaPraticaGit/CD.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaProvaPraticaGit
+{
+    internal class CD
+    {
+        internal List<Brano> ListaDeiBrani = new List<Brano>();
+        internal string Titolo { get; set; }
+        internal string Autore { get; set; }
+
+        internal CD (List<Brano> ListaDeiBrani, string Titolo, string Autore)
+        {
+            this.ListaDeiBrani = ListaDeiBrani;
+            this.Titolo = Titolo;
+            this.Autore = Autore;
+        }
+
+        internal toString (List<Brano> ListaDeiBrani)
+        {
+            StringBuilder sb = new StringBuilder ();
+            for (int i = 0; i < ListaDeiBrani.Count; i++)
+            {
+                sb = ListaDeiBrani [i].ToString ();
+            }
+        }
+    }
+}
