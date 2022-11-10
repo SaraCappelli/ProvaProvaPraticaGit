ad08fede011cb96539e3fd112517532da2941560
Sara Cappelli <SARA.CAPPELLI@local.ittcesena.it>
2022-11-10 09:17:11 +0100

    Interfaccia e main 1.0: creazione di brani e cd, manca il check che il titolo e l'autore siano stati effettivamente compilati

--- ProvaProvaPraticaGit/MainWindow.xaml.cs
+++ ProvaProvaPraticaGit/MainWindow.xaml.cs
@@ -24,5 +24,51 @@ namespace ProvaProvaPraticaGit
         {
             InitializeComponent();
         }
+
+        List<Brano> listaBrani = new List<Brano>();
+        private void creaBrano_button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Brano b = new Brano(titolo_txtBox.Text, autore_txtBox.Text, MinSecToSec(durata_txtBox.Text));
+                listaBrani.Add(b);
+                brani_listBox.Items.Add(b);
+            } catch
+            {
+
+            }
+        }
+
+        private int MinSecToSec(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private void creaCD_button_Click(object sender, RoutedEventArgs e)
+        {
+            CD cd = new CD(titoloCD_txtBox, autoreCD_txtBox, listaBrani);
+            listaBrani.Clear();
+            brani_listBox.Items.Clear();
+            listaCD_listBox.Items.Add(cd);
+        }
+
+
+
+        private void listaCD_listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                CD cdSel = listaCD_listBox.SelectedItem;
+                listaBraniCD_listBox.Items.Clear();
+                foreach (Brano b in cdSel.ListaDeiBrani)
+                {
+                    listaBraniCD_listBox.Items.Add(b);
+                }
+            } catch
+            {
+
+            }
+        }
     }
 }
